b72b4e80fb0636184d7cf5ab7d126850afb085a4
ruthless2skech <74581915+manesh1234@users.noreply.github.com>
2023-01-29 18:27:09 +0530

    structuring
    structuring

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
     return (
         <nav>
             <a href="/" onClick={() => setState('/')} className={activeState === '#' ? "active" : ''} title="home"><AiFillHome /></a>
-            <a href="#about" onClick={() => setState('#about')} className={activeState === '#about' ? "active": ''} title="about"><AiOutlineUser /></a>
+            <a href="#about" onClick={() => setState('#about')} className={activeState === '#about' ? "active" : ''} title="about"><AiOutlineUser /></a>
             <a href="#experience" onClick={() => setState('#experience')} className={activeState === '#experience' ? "active" : ''} title="experience"><FaBook /></a>
             <a href="#projects" onClick={() => setState('#services')} className={activeState === '#projects' ? "active" : ''} title="projects"><FcServices /></a>
             <a href="#contact" onClick={() => setState('#contact')} className={activeState === '#contact' ? "active" : ''} title="contact"><BsMessenger /></a>
